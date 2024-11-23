3d8033c76186b143b8aad708d80c6e52175672e3
kamau <githuakamau99@gmail.com>
2024-11-23 21:57:21 +0300

    feat: implement messaging functionality and user sidebar retrieval
    - Add  controller to fetch all users except the logged-in user, excluding password fields.
    - Add  controller to retrieve chat messages between the logged-in user and a specific user.
    - Add  controller to enable sending messages, including optional image uploads to Cloudinary.
    - Emit real-time notifications for new messages using Socket.IO.
    - Handle error scenarios in all controllers with appropriate responses.
    - Integrate Cloudinary for secure image storage and Socket.IO for real-time updates.

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import {connectDB} from "./lib/db.js"
 
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 
 dotenv.config();
 const app = express();
@@ -17,6 +18,7 @@ app.use(cookieParser());
 
 // auth routes
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 
 
